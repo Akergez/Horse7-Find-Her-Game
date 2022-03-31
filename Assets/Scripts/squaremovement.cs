bd93efe879cd168ca894e3865a31f457cee23807
Akergez <ragusseven@gmail.com>
2022-03-31 18:21:23 +0500

    Add all files

---
+++ Assets/Scripts/squaremovement.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class squaremovement : MonoBehaviour
+{
+    private const float Acceleration = 3;
+
+    private Rigidbody2D _rigidBodyComponent;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _rigidBodyComponent = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var w = Input.GetKey(KeyCode.W) ? 1 : 0;
+        var s = Input.GetKey(KeyCode.S) ? -1 : 0;
+        var a = Input.GetKey(KeyCode.A) ? 1 : 0;
+        var d = Input.GetKey(KeyCode.D) ? -1 : 0;
+        var space = Input.GetKey(KeyCode.Space) ? -1 : 0;
+        var movementVector = new Vector2(-(a + d), w + s);
+        _rigidBodyComponent.velocity = movementVector * Acceleration;
+        if (!(movementVector.magnitude > Mathf.Epsilon)) return;
+        var angle = new Vector3(0 ,0, movementVector.GetAngle());
+        transform.rotation = Quaternion.Euler(angle);
+
+    }
+}

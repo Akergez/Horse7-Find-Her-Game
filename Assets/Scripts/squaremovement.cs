884cc3460ef87004306706ee7c3ba9d21eda0d0f
Akergez <ragusseven@gmail.com>
2022-03-31 22:21:20 +0500

    fix

--- Assets/Scripts/squaremovement.cs
+++ Assets/Scripts/squaremovement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class squaremovement : MonoBehaviour
 {
-    private const float Acceleration = 3;
+    private const float Acceleration = 1;
 
     private Rigidbody2D _rigidBodyComponent;
     // Start is called before the first frame update
@@ -14,15 +14,16 @@ public class squaremovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        var w = Input.GetKey(KeyCode.W) ? 1 : 0;
-        var s = Input.GetKey(KeyCode.S) ? -1 : 0;
-        var a = Input.GetKey(KeyCode.A) ? 1 : 0;
-        var d = Input.GetKey(KeyCode.D) ? -1 : 0;
+        var w = Input.GetKey(KeyCode.W) ? 0.1 : 0;
+        var s = Input.GetKey(KeyCode.S) ? -0.1 : 0;
+        var a = Input.GetKey(KeyCode.A) ? 0.1 : 0;
+        var d = Input.GetKey(KeyCode.D) ? -0.1 : 0;
         var space = Input.GetKey(KeyCode.Space) ? -1 : 0;
-        var movementVector = new Vector2(-(a + d), w + s);
+        var movementVector = new Vector2(-((float)a + (float)d), (float)w + (float)s);
+        movementVector.Normalize();
         _rigidBodyComponent.velocity = movementVector * Acceleration;
         if (!(movementVector.magnitude > Mathf.Epsilon)) return;
-        var angle = new Vector3(0 ,0, movementVector.GetAngle());
+        var angle = new Vector3(0 ,0, 0);
         transform.rotation = Quaternion.Euler(angle);
 
     }

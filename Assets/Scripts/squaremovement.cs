d25e76af6d0d1e8fca363105767a7f9476f23345
senyapekus <ksenya.pshenichnikova03@gmail.com>
2022-04-13 20:40:01 +0500

    fix errors in menu and pause

--- Assets/Scripts/squaremovement.cs
+++ Assets/Scripts/squaremovement.cs
@@ -30,19 +30,4 @@ public class squaremovement : MonoBehaviour
         transform.rotation = Quaternion.Euler(angle);
 
     }
-
-    public static void LoadGame()
-    {
-        if (File.Exists(Application.persistentDataPath + "/MySaveData.dat"))
-        {
-            BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/MySaveData.dat", FileMode.Open);
-            SaveData data = (SaveData)bf.Deserialize(file);
-            file.Close();
-            GameObject.Find("Isometric Diamond").transform.position = new Vector3(data.position[0], data.position[1], data.position[2]);
-            Debug.Log("Game data loaded!");
-        }
-        else
-            Debug.LogError("There is no save data!");
-    }
 }

67517598e6c3c0438fb3bd9e903c8b3fba59d810
Lada Kuzmina <lada.kuzmina2003@gmail.com>
2022-04-14 20:02:00 +0500

    новая локация + дырок нет в общаге + стул по размерам больше

---
+++ Assets/Camera.cs
@@ -0,0 +1,20 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = player.transform.position;
+        transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+    }
+}*/
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -29,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
         var s = Input.GetKey(KeyCode.S) ? -0.1 : 0;
         var a = Input.GetKey(KeyCode.A) ? 0.1 : 0;
         var d = Input.GetKey(KeyCode.D) ? -0.1 : 0;
+
         movementVector = new Vector2(-((float) a + (float) d), (float) w + (float) s);
         movementVector.Normalize();
 

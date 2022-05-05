f46e4bc16f868aa14877cc07b683799b821c6fad
Akergez <ragusseven@gmail.com>
2022-05-05 19:17:11 +0500

    add_magic_circle

--- Assets/Scripts/Vector3Extensions.cs
+++ Assets/Scripts/Vector3Extensions.cs
@@ -8,4 +8,9 @@ public static class Vector3Extensions
     {
         return math.sqrt(vector3.x * vector3.x + vector3.y * vector3.y + vector3.z * vector3.z);
     }
+
+    public static Vector3 Leeeeeeength(this Vector3 vector3, double scalar)
+    {
+        return new Vector3(vector3.x * (float)scalar, vector3.y * (float)scalar, vector3.z * (float)scalar);
+    }
 }

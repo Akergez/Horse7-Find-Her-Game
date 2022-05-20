d10a7d4b129cfd685c3bd2cdd7d2253c6d8633f3
Akergez <ragusseven@gmail.com>
2022-05-20 07:32:51 +0500

    return some useless scripts

---
+++ Assets/Scripts/Player Behaviour/PlayerData.cs
@@ -0,0 +1,15 @@
+[System.Serializable]
+public class PlayerData
+{
+    public int hungry;
+    public int health;
+    public float[] position;
+
+    public PlayerData(squaremovement player)
+    {
+        position = new float[3];
+        position[0] = player.transform.position.x;
+        position[1] = player.transform.position.y;
+        position[2] = player.transform.position.z;
+    }
+}
